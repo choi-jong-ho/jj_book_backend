398c1967b8ec2dbb11a841a7866e6a141f149946
coffee <dlsnditi751@gmail.com>
2023-07-26 11:24:43 +0900

    TEST 커밋

---
+++ src/test/java/com/example/jj_book/JjBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jj_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
