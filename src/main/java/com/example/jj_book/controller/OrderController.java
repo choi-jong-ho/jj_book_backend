890682d1f1ca0728127ea28f02ce6085f8fadd4f
coffee <dlsnditi751@gmail.com>
2023-08-16 13:53:39 +0900

    20230815 주문 조회 추가

--- src/main/java/com/example/jj_book/controller/OrderController.java
+++ src/main/java/com/example/jj_book/controller/OrderController.java
@@ -1,22 +1,25 @@
 package com.example.jj_book.controller;
 
 import com.example.jj_book.dto.OrderDto;
+import com.example.jj_book.dto.OrderHistDto;
 import com.example.jj_book.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class OrderController {
 
@@ -25,6 +28,9 @@ public class OrderController {
     @PostMapping(value = "/order")
     public @ResponseBody ResponseEntity order(@RequestBody @Valid OrderDto orderDto
             , BindingResult bindingResult, Principal principal) {
+
+        System.out.println("getItemId :" + orderDto.getItemId());
+        System.out.println("getCount :" + orderDto.getCount());
 
         if (bindingResult.hasErrors()) {
             StringBuilder sb = new StringBuilder();
@@ -50,4 +56,18 @@ public class OrderController {
 
         return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
+
+    //구매 이력 조회
+    @GetMapping(value = {"/order/list", "/order/list/{page}"})
+    public List<Page> orderHist(@PathVariable("page") Optional<Integer> page, Principal principal){
+
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 10);
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        List<Page> list = new ArrayList<>();
+        list.add(orderHistDtoList);
+
+        return list;
+    }
+
 }
--- src/main/java/com/example/jj_book/service/OrderService.java
+++ src/main/java/com/example/jj_book/service/OrderService.java
@@ -1,15 +1,17 @@
 package com.example.jj_book.service;
 
 import com.example.jj_book.dto.OrderDto;
-import com.example.jj_book.entity.Item;
-import com.example.jj_book.entity.Member;
-import com.example.jj_book.entity.Order;
-import com.example.jj_book.entity.OrderItem;
+import com.example.jj_book.dto.OrderHistDto;
+import com.example.jj_book.dto.OrderItemDto;
+import com.example.jj_book.entity.*;
 import com.example.jj_book.repo.ItemImgRepository;
 import com.example.jj_book.repo.ItemRepository;
 import com.example.jj_book.repo.MemberRepository;
 import com.example.jj_book.repo.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -44,5 +46,29 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable){
+
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        //주문 리스트 순회 -> 구매 이력페이지에 전달
+        for(Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems();
+            for(OrderItem orderItem : orderItems){
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem().getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<OrderHistDto>(orderHistDtos, pageable, totalCount);
     }
 }
